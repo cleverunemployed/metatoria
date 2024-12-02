216d7924334d13a07536e5d53f71815e884d6a3f
DjonniStorm <i.pazushkin@yandex.ru>
2024-12-03 02:22:17 +0400

    ✨ sorry for pushing in main"

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,13 @@ import './App.css'
 import { AboutUs } from './template/AboutUs/AboutUs';
 import { CurrentProjects } from './template/CurrentProjects/CurrentProjects';
 import Header from './template/header/Header'
-import { WhyWe } from './template/WhyWe/WhyWe';
 
 export default function App() {
   return (
     <>
       {/* <Header /> */}
       {/* <AboutUs /> */}
-      {/* <CurrentProjects /> */}
-      <WhyWe />
+      <CurrentProjects />
     </>
   )
 }
