c70bb0f07b58a2e8d4a8d9ea94fc203b1e351741
cleverunemployed <agonycavarly@gmail.com>
2024-12-02 15:48:43 +0400

    to add tailwind in project

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 
 import './App.css'
+import Header from './template/header/Header'
 
 export default function App() {
   return (
     <>
-
+      <Header></Header>
     </>
   )
 }
---
+++ src/template/header/Header.jsx
@@ -0,0 +1,7 @@
+export default function Header() {
+    return (
+      <div className="h-56 w-[100%] bg-slate-600">
+
+      </div>
+    )
+  }
