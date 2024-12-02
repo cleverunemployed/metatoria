fe01b760d021e198c6b62d01955d4ce405bf4214
DjonniStorm <i.pazushkin@yandex.ru>
2024-12-03 02:18:04 +0400

    ✨ spend more on bug, than on worl

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,15 @@ import './App.css'
 import { AboutUs } from './template/AboutUs/AboutUs';
 import { CurrentProjects } from './template/CurrentProjects/CurrentProjects';
 import Header from './template/header/Header'
+import { WhyWe } from './template/WhyWe/WhyWe';
 
 export default function App() {
   return (
     <>
       {/* <Header /> */}
       {/* <AboutUs /> */}
-      <CurrentProjects />
+      {/* <CurrentProjects /> */}
+      <WhyWe />
     </>
   )
 }
---
+++ src/template/WhyWe/WhyWe.jsx
@@ -0,0 +1,15 @@
+export const WhyWe = () => (
+    <div className="min-h-screen w-full">
+        <div className="text-6xl pt-16 pb-8">
+            <h2 className="text-center">Почему именно мы?</h2>
+        </div>
+        <div className="mt-10 mx-10 px-5 pt-12 pb-8 bg-gray-200/70 rounded-[15px] shadow-[4px_4px_4px_-1px_rgba(0,0,0,0.3)]">
+            <ul className="list-none list-outside hover:list-inside font-normal text-[30px] space-y-10">
+                <li>Реализовали и отрисовали три локации всего за пару месяцев</li>
+                <li>Мы победители Всероссийского конкурса молодежных проектов в 2024 году</li>
+                <li>Проведение мастер-классов и конференций для студентов и школьников</li>
+                <li>Регулярно проводим профориентации: 200 школьников уже побывали и познакомились с XR-технологиями в лаборатории «Метатория»</li>
+            </ul>
+        </div>
+    </div>
+);
