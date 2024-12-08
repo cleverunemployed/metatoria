d70c9942e3f2e1a72f5cfe61a683a9ba7d980d99
DjonniStorm <i.pazushkin@yandex.ru>
2024-12-08 23:43:06 +0400

    v0.0.1

--- src/template/FormPanel/FormPanel.jsx
+++ src/template/FormPanel/FormPanel.jsx
@@ -1,53 +1,96 @@
-import { useState } from 'react';
-import VRGlassesOnTable from '../../image/vr-glasses-on-table.png';
+import { useRef, useState } from "react";
+import VRGlassesOnTable from "../../image/vr-glasses-on-table.png";
 
 export const FormPanel = () => {
+    const phoneNumberMask = "+7(___)___-__-__";
 
-    const [ telInput, setTelInput ] = useState(() => '');
+    const [phoneNumberInput, setPhoneNumberInput] = useState(() => "");
 
-    const handleTelInput = (event) => {
-        console.log(event.target.value);
+    const phoneInputRef = useRef(null);
 
-        /* TODO: validate inputs */
+    const handlePhoneNumberInput = (event) => {
+        const { value, selectionStart, selectionEnd } = event.target;
+        /* TODO: normal validate inputs */
+        const input = value.replace(/^\+7|[^\d]/g, "");
+        let formattedValue = phoneNumberMask;
+        console.log(value, input);
 
-        setTelInput(event.target.value);
-    }
+        Array.from(input).forEach((digit) => {
+            formattedValue = formattedValue.replace("_", digit);
+        });
+
+        setPhoneNumberInput(formattedValue);
+
+        // just for re-render
+        setTimeout(() => {
+            if (phoneInputRef.current) {
+                const pos =
+                    formattedValue.indexOf(
+                        value[selectionStart - 1],
+                        selectionEnd - 1
+                    ) + 1 || selectionEnd;
+                phoneInputRef.current.setSelectionRange(pos, pos);
+            }
+        }, 0);
+    };
 
     return (
-        <div className="w-full h-screen">
-            <div className='m-10'>
-                <h2 className='text-center text-6xl'>Заполнить анкету</h2>
+        <div className="w-full h-screen grid grid-rows-[auto_1fr]">
+            <div className="m-10">
+                <h2 className="text-center text-6xl">Заполнить анкету</h2>
             </div>
-            <div className='flex'>
-                <div className="flex-1 flex">
-                    <form className='w-[90%] flex flex-col gap-10 items-center justify-center'>
+            <div className="flex justify-around">
+                <div className="flex-1 flex justify-center">
+                    <form
+                        style={{ backgroundColor: "#F2EFEF4D" }}
+                        className="w-[80%] h-5/6 p-10 flex flex-col gap-12 items-center justify-center rounded-[45px] shadow-[4px_4px_4px_-1px_rgba(0,0,0,0.3)]"
+                    >
                         <input
-                            className='w-4/5' 
-                            type='text' 
-                            placeholder='Иванов Иван'
-                            required />
-                        <input 
-                            className='w-4/5'
-                            type='tel' 
-                            name='phoneNumber' 
-                            placeholder='+7(___)___-__-__' 
-                            pattern="^\+7$\d{3}$\d{3}-\d{2}-\d{2}$" value={telInput} 
-                            onChange={handleTelInput}
-                            required />
+                            className="w-full h-2/6 text-center text-5xl rounded-3xl border-4 border-black placeholder:text-center placeholder:text-5xl placeholder:text-black focus:placeholder:text-transparent"
+                            style={{ backgroundColor: "#ACC5F8" }}
+                            type="text"
+                            placeholder="Иванов Иван"
+                            required
+                        />
+                        <input
+                            ref={phoneInputRef}
+                            className="h-2/6 w-full text-center text-3xl rounded-3xl border-4 border-black placeholder:text-center placeholder:text-4xl placeholder:text-black focus:placeholder:text-transparent"
+                            type="tel"
+                            name="phoneNumber"
+                            style={{ backgroundColor: "#ACC5F8" }}
+                            placeholder={phoneNumberMask}
+                            // pattern="^\+7$\d{3}$\d{3}-\d{2}-\d{2}$"
+                            value={
+                                phoneNumberInput === ""
+                                    ? phoneNumberMask
+                                    : phoneNumberInput
+                            }
+                            onChange={handlePhoneNumberInput}
+                            required
+                        />
                         <textarea
-                            className='w-4/5 resize-none'
-                            type='text' 
-                            placeholder='Комментарий'
-                            required />
-                        <button type='submit'>Отправить</button>
+                            className="resize-none h-2/6 w-full text-center text-3xl rounded-3xl border-4 border-black placeholder:p-6 placeholder:text-center placeholder:text-4xl placeholder:text-black focus:placeholder:text-transparent"
+                            style={{ backgroundColor: "#ACC5F8" }}
+                            type="text"
+                            placeholder="Комментарий"
+                        />
+                        <button
+                            className="h-2/6 w-full text-center text-3xl rounded-3xl border-4 border-black"
+                            style={{ backgroundColor: "#907DFFB2" }}
+                            type="submit"
+                        >
+                            Отправить
+                        </button>
                     </form>
                 </div>
-                <div className="flex-1 flex flex-col items-start justify-center">
-                    <div className='max-w-[80%] rounded-xl overflow-hidden rotate-[6deg]'>
-                        <img className='' src={VRGlassesOnTable} alt="vr glasses" />
-                    </div>
+                <div className="flex-1 flex items-center justify-center">
+                    <img
+                        className="rounded-xl max-w-[80%] max-h-[70%] rotate-[6deg] mx-auto"
+                        src={VRGlassesOnTable}
+                        alt="vr glasses"
+                    />
                 </div>
             </div>
         </div>
     );
-}
+};
