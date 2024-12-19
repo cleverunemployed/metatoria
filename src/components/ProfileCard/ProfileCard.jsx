8077119892a2bf32935d3e6d305f67259efbbb03
DjonniStorm <i.pazushkin@yandex.ru>
2024-12-19 18:53:06 +0400

    add base grid for team view

--- src/components/ProfileCard/ProfileCard.jsx
+++ src/components/ProfileCard/ProfileCard.jsx
@@ -1,11 +1,15 @@
 export const ProfileCard = ({ imgSource, className = "", person, role }) => (
     <div
-        className={`bg-[#F2EFEF4D] rounded-[45px] flex flex-col justify-center items-center shadow-[4px_4px_4px_-1px_rgba(0,0,0,0.3)] ${className}`}
+        className={`bg-[#F2EFEF4D] max-h-80 rounded-[45px] flex flex-col justify-center items-center shadow-[4px_4px_4px_-1px_rgba(0,0,0,0.3)] ${className}`}
     >
-        <div className="overflow-hidden rounded-xl w-5/6 m-5">
-            <img src={imgSource} alt="" className="" />
+        <div className="overflow-hidden w-5/6 aspect-square pt-1 px-5 pb-1 flex m-auto">
+            <img
+                src={imgSource}
+                alt={person}
+                className="rounded-xl object-cover"
+            />
         </div>
-        <div className="">
+        <div className="max-w-[90%]">
             <h3 className="text-center text-[#1652D1] font-extrabold font-montserrat">
                 {person}
             </h3>
--- src/template/OurTeam/OurTeam.jsx
+++ src/template/OurTeam/OurTeam.jsx
@@ -1,53 +1,60 @@
 import { ProfileCard } from "../../components/ProfileCard/ProfileCard";
-import DEdremov from "../../image/chief-efremov-d.jpg";
-import GuskovGleb from "../../image/guskov-gleb.jpg";
-import KadyrovaDinara from "../../image/kadyrova-dinara.jpg";
-import FedorinAndrey from "../../image/fedorin-andrey.jpg";
-import VatlinVyacheslav from "../../image/vatlin-vyacheslav.jpg";
-import KlimentovaAnastasia from "../../image/klimentova-anastasia.jpg";
-import DolgovKirill from "../../image/dolgov-kirill.jpg";
+import DEdremov from "../../image/chief-efremov-d.png";
+import GuskovGleb from "../../image/guskov-gleb.png";
+import KadyrovaDinara from "../../image/kadyrova-dinara.png";
+import FedorinAndrey from "../../image/fedorin-andrey.png";
+import VatlinVyacheslav from "../../image/vatlin-vyacheslav.png";
+import KlimentovaAnastasia from "../../image/klimentova-anastasia.png";
+import DolgovKirill from "../../image/dolgov-kirill.png";
 
 export const OurTeam = () => {
     return (
         <div className="w-full h-screen">
             <div className="flex justify-center items-center">
-                <h2>Наша команда</h2>
+                <h2 className="text-center text-6xl">Наша команда</h2>
             </div>
-            <div className="grid grid-cols-4 grid-rows-2 gap-10">
+            <div className="grid grid-cols-8 gap-10 p-10 max-h-5/6">
                 <ProfileCard
                     imgSource={DEdremov}
                     person={"Ефремов Денис"}
                     role={"Автор и куратор проекта"}
+                    className="col-span-2 row-start-1"
                 />
                 <ProfileCard
                     imgSource={GuskovGleb}
                     person={"Гуськов Глеб"}
                     role={"Научный руководитель"}
+                    className="col-span-2 row-start-1"
                 />
                 <ProfileCard
                     imgSource={KadyrovaDinara}
                     person={"Кадырова Динара"}
                     role={"Руководитель лаборатории"}
+                    className="col-span-2 row-start-1"
                 />
                 <ProfileCard
                     imgSource={FedorinAndrey}
                     person={"Федорин Андрей"}
                     role={"3D-художник"}
+                    className="col-span-2 row-start-1"
                 />
                 <ProfileCard
                     imgSource={VatlinVyacheslav}
                     person={"Ватлин Владислав"}
                     role={"Ведущий разработчик"}
+                    className="col-span-2 row-start-2 col-start-2"
                 />
                 <ProfileCard
                     imgSource={KlimentovaAnastasia}
                     person={"Климентова Анастасия"}
                     role={"Сценарист"}
+                    className="col-span-2 row-start-2 col-start-4"
                 />
                 <ProfileCard
                     imgSource={DolgovKirill}
                     person={"Долгов Кирилл"}
                     role={"Системный администратор"}
+                    className="col-span-2 row-start-2 col-start-6"
                 />
             </div>
         </div>
